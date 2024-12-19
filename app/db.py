6d55538809553ee8d30c45a4cde5f8b686492c66
Maxim Morozov <Lewisdew@bk.ru>
2024-12-19 19:55:25 +0300

    add env variables to db.py

--- app/db.py
+++ app/db.py
@@ -1,12 +1,14 @@
+import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
+# Чтение конфигурации из переменных окружения
 DATABASE_CONFIG = {
-    "dbname": "3lrar_db",
-    "user": "usermaxim",
-    "password": "110622",
-    "host": "localhost",
-    "port": 5432,
+    "dbname": os.getenv("DB_NAME", "default_db"),
+    "user": os.getenv("DB_USER", "default_user"),
+    "password": os.getenv("DB_PASSWORD", "default_password"),
+    "host": os.getenv("DB_HOST", "localhost"),
+    "port": int(os.getenv("DB_PORT", 5432)),  # Преобразуем порт в число
 }
 
 def get_db_connection():
